6114cc5f7336baa8f435df2bfd0493fedd9b6099
Rossy167 <37879439+Rossy167@users.noreply.github.com>
2020-05-17 01:33:50 +0100

    Added C# UI

---
+++ UI/Steam-Blocker-UI/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Diagnostics;
+
+namespace Steam_Blocker_UI
+{
+    public partial class MainWindow : Window
+    {
+        private string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void runPSCommand(string command)
+        { 
+            using (Process cmd = new Process())
+            {
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+                cmd.Start();
+
+                cmd.StandardInput.WriteLine("powershell -command " + command);
+                cmd.StandardInput.Flush();
+
+                cmd.StandardInput.Close();
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            runPSCommand("Block-Steam");
+            CommandTextBox.Text = "Steam has been blocked";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            runPSCommand("Unblock-Steam");
+            CommandTextBox.Text = "Steam has been unblocked";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            runPSCommand("cd " + currentDir);
+            runPSCommand(".\\Save-BlockSteamToProfile.ps1");
+            CommandTextBox.Text = "The other two buttons will now always work";
+        }
+    }
+}
--- scripts/Save-BlockSteamToProfile.ps1
+++ scripts/Save-BlockSteamToProfile.ps1
@@ -1,4 +1,5 @@
-Add-Content -Path $PROFILE -Value "function Block-Steam {New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Common Files\Steam\SteamService.exe' -Profile any -Direction Outbound -Displayname 'Block-Steam Rossy' | Out-Null
+Add-Content -Path $PROFILE -Value "
+function Block-Steam {New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Common Files\Steam\SteamService.exe' -Profile any -Direction Outbound -Displayname 'Block-Steam Rossy' | Out-Null
 New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Steam\bin\cef\cef.win7x64\steamwebhelper.exe' -Profile any -Direction Outbound -Displayname 'Block-Steam Rossy' | Out-Null
 New-NetFirewallRule -Action block -Program 'C:\program files (x86)\steam\steam.exe' -Profile any -direction Outbound -Displayname 'Block-Steam Rossy' | Out-Null
 New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Common Files\Steam\SteamService.exe' -Profile any -Direction Inbound -Displayname 'Block-Steam Rossy' | Out-Null
